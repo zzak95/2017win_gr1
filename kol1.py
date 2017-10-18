83adc659774e9db60040a58efcf0715d53317e78
mwmajew <maciej.w.majewski@gmail.com>
2017-10-18 17:55:23 +0200

    Create kol1.py

---
+++ kol1.py
@@ -0,0 +1,13 @@
+#Banking simulator. Write a code in python that simulates the banking system. 
+#The program should:
+# - be able to create new banks
+# - store client information in banks
+# - allow for cash input and withdrawal
+# - allow for money transfer from client to client
+#If you can thing of any other features, you can add them.
+#This code shoud be runnable with 'python kol1.py'.
+#You don't need to use user input, just show me in the script that the structure of your code works.
+#If you have spare time you can implement: Command Line Interface, some kind of data storage, or even multiprocessing.
+#Do your best, show off with good, clean, well structured code - this is more important than number of features.
+#After you finish, be sure to UPLOAD this (add, commit, push) to the remote repository.
+#Good Luck
